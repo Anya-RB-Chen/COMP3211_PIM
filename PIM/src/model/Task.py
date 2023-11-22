918cb3b6c6cb2046bb406ffcc538697be597ffcf
Bruce LU <121073119+LuZhoudao@users.noreply.github.com>
2023-11-22 11:47:32 +0800

    Update Task.py
    task加入reminder的比较

--- PIM/src/model/Task.py
+++ PIM/src/model/Task.py
@@ -52,11 +52,11 @@ class Task(PIM):
 
     def time_condition_checker(self, time: float, comparator: str):
         if comparator == "<":
-            return time > Tools.timeStr_to_timeStamp(self.deadline)
+            return time > Tools.timeStr_to_timeStamp(self.deadline) or time > Tools.timeStr_to_timeStamp(self.reminder)
         elif comparator == "=":
-            return time ==  Tools.timeStr_to_timeStamp(self.deadline)
+            return time == Tools.timeStr_to_timeStamp(self.deadline) or time == Tools.timeStr_to_timeStamp(self.reminder)
         elif comparator == ">":
-            return time <  Tools.timeStr_to_timeStamp(self.deadline)
+            return time < Tools.timeStr_to_timeStamp(self.deadline) or time < Tools.timeStr_to_timeStamp(self.reminder)
         else:
             return False
 
